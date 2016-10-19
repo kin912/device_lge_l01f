457bef6c633345c7a4a6ced9ab9ae74adb7d6ab4
kin912 <1718251@qq.com>
2016-10-19 14:38:54 +0800

    add l01f device

---
+++ l01f.mk
@@ -0,0 +1,27 @@
+#
+# Copyright (C) 2016 The Paranoid Android Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := device/lge/l01f
+
+$(call inherit-product, device/lge/g2-common/common.mk)
+$(call inherit-product, vendor/lge/l01f/l01f-vendor.mk)
+
+## Overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# Sensors
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensor_def_l01f.conf:system/etc/sensor_def_variable.conf
